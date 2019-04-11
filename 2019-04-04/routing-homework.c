eae52a81a67ee56b1b432596e80f42c57a3e9939
Poe <piotr.a.ptak@icloud.com>
2019-04-11 11:56:40 +0200

    Add dynamic routing homework

--- 2019-04-04/routing-homework.c
+++ 2019-04-04/routing-homework.c
@@ -1,8 +1,8 @@
 /*
  * Copyright (C) 2019 Michal Kalewski <mkalewski at cs.put.poznan.pl>
  *
- * Compilation:  gcc -Wall ./routing.c -o ./routing
- * Usage:        ./routing INTERFACE
+ * Compilation:  gcc -Wall ./ethrecv.c -o ./ethrecv
+ * Usage:        ./ethrecv INTERFACE
  * NOTE:         This program requires root privileges.
  *
  * Bug reports:  https://gitlab.cs.put.poznan.pl/mkalewski/ps-2019/issues
@@ -13,6 +13,7 @@
 #include <linux/if_arp.h>
 #include <linux/if_ether.h>
 #include <linux/if_packet.h>
+#include <linux/route.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -20,6 +21,8 @@
 #include <unistd.h>
 
 #define ETH_P_CUSTOM 0x8888
+#define IRI_T_ADDRESS 0
+#define IRI_T_ROUTE 1
 
 // odbieramy ramki 0x8888. w tych ramkach bedzie struktura ifrtinfo. ta
 // struktura bedzei wymagala skonfigurowania dresu ip na interfejsie albo
@@ -31,33 +34,13 @@
 // 1 to otrzymamy adres sieci, maske i brame (3 ostatnie pola w strukturze)
 
 struct ifrtinfo {
-  int iri_type;
-  char iri_iname[16];
-  struct sockaddr_in iri_iaddr; /* IP address */
+  int iri_type;                 /* msg type  */
+  char iri_iname[16];           /* ifname  */
+  struct sockaddr_in iri_iaddr; /* IP address  */
   struct sockaddr_in iri_rtdst; /* dst. IP address */
-  struct sockaddr_in iri_rtmsk; /* dst. netmask */
+  struct sockaddr_in iri_rtmsk; /* dst. netmask  */
   struct sockaddr_in iri_rtgip; /* gateway IP */
 };
-
-int ifsetup(char* IFNAME, char* IP) {
-  int sfd;
-  struct ifreq ifr;
-  struct sockaddr_in* sin;
-
-  sfd = socket(PF_INET, SOCK_DGRAM, 0);
-  strncpy(ifr.ifr_name, IFNAME, strlen(IFNAME) + 1);
-  sin = (struct sockaddr_in*)&ifr.ifr_addr;
-  memset(sin, 0, sizeof(struct sockaddr_in));
-  sin->sin_family = AF_INET;
-  sin->sin_port = 0;
-  sin->sin_addr.s_addr = inet_addr(IP);
-  ioctl(sfd, SIOCSIFADDR, &ifr);
-  ioctl(sfd, SIOCGIFFLAGS, &ifr);
-  ifr.ifr_flags |= IFF_UP | IFF_RUNNING;
-  ioctl(sfd, SIOCSIFFLAGS, &ifr);
-  close(sfd);
-  return EXIT_SUCCESS;
-}
 
 int main(int argc, char** argv) {
   int sfd, i;
@@ -85,14 +68,34 @@ int main(int argc, char** argv) {
     frame = malloc(ETH_FRAME_LEN);
     memset(frame, 0, ETH_FRAME_LEN);
     fhead = (struct ethhdr*)frame;
-    fdata = frame + ETH_HLEN;
+    // fdata = frame + ETH_HLEN;
     len = recvfrom(sfd, frame, ETH_FRAME_LEN, 0, NULL, NULL);
-    r = (struct ifrtinfo*)frame + ETH_HLEN;
-    printf("iri_type: %d\n", r->iri_type);
+    r = (struct ifrtinfo*)(frame + ETH_HLEN);
 
-    if (r->iri_type == 0) {
-      ifsetup(r->iri_iname, r->iri_iaddr);
-    } else if (r->iri_type == 1) {
+    printf("Type: %d\n", r->iri_type);
+    if (r->iri_type == IRI_T_ADDRESS) {
+      printf("[IRI_T_ADDRESS]\n");
+
+      int sfd;
+      struct ifreq ifr;
+      struct sockaddr_in* sin;
+
+      sfd = socket(PF_INET, SOCK_DGRAM, 0);
+      strncpy(ifr.ifr_name, r->iri_iname, strlen(r->iri_iname) + 1);
+      sin = (struct sockaddr_in*)&ifr.ifr_addr;
+      memset(sin, 0, sizeof(struct sockaddr_in));
+      sin->sin_family = AF_INET;
+      sin->sin_port = 0;
+      sin->sin_addr.s_addr = r->iri_iaddr.sin_addr.s_addr;
+      ioctl(sfd, SIOCSIFADDR, &ifr);
+      ioctl(sfd, SIOCGIFFLAGS, &ifr);
+      ifr.ifr_flags |= IFF_UP | IFF_RUNNING;
+      ioctl(sfd, SIOCSIFFLAGS, &ifr);
+      close(sfd);
+
+    } else if (r->iri_type == IRI_T_ROUTE) {
+      printf("[IRI_T_ROUTE]\n");
+
       int sfd;
       struct rtentry route;
       struct sockaddr_in* addr;
@@ -101,15 +104,15 @@ int main(int argc, char** argv) {
       memset(&route, 0, sizeof(route));
       addr = (struct sockaddr_in*)&route.rt_gateway;
       addr->sin_family = AF_INET;
-      addr->sin_addr = r->iri_rtdst;
+      addr->sin_addr.s_addr = r->iri_rtgip.sin_addr.s_addr;
       addr = (struct sockaddr_in*)&route.rt_dst;
       addr->sin_family = AF_INET;
-      addr->sin_addr.s_addr = INADDR_ANY;
+      addr->sin_addr.s_addr = r->iri_rtdst.sin_addr.s_addr;
       addr = (struct sockaddr_in*)&route.rt_genmask;
       addr->sin_family = AF_INET;
-      addr->sin_addr = r->iri_rtmsk.sin_addr;
+      addr->sin_addr.s_addr = r->iri_rtmsk.sin_addr.s_addr;
       route.rt_flags = RTF_UP | RTF_GATEWAY;
-      route.rt_metric = 500;
+      route.rt_metric = 0;
       ioctl(sfd, SIOCADDRT, &route);
       close(sfd);
     }
@@ -120,7 +123,7 @@ int main(int argc, char** argv) {
     printf("%02x:%02x:%02x:%02x:%02x:%02x | ", fhead->h_dest[0],
            fhead->h_dest[1], fhead->h_dest[2], fhead->h_dest[3],
            fhead->h_dest[4], fhead->h_dest[5]);
-    printf("%s\n", fdata);
+    printf("%s\n", r);
     for (i = 0; i < len; i++) {
       printf("%02x ", (unsigned char)frame[i]);
       if ((i + 1) % 16 == 0) printf("\n");
